c73c1eb30a20856c604b40af64e2850dab9e5610
Hitmmm <151015933+Hitmmm@users.noreply.github.com>
2026-03-03 14:31:47 +0300

    Add Firebase configuration for Briz-Messenger

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Briz-Messenger Global Connection Key
+const firebaseConfig = {
+  apiKey: "AIzaSyB8emEQNjFrizdvb6i2jftipgKROB_T6Q4",
+  authDomain: "briz-messenger.firebaseapp.com",
+  projectId: "briz-messenger",
+  storageBucket: "briz-messenger.firebasestorage.app",
+  messagingSenderId: "334191649483",
+  appId: "1:334191649483:web:741fd0808907b3e0a6b14e",
+  measurementId: "G-TTCGC9RP0S"
+};
+
+export default firebaseConfig;
